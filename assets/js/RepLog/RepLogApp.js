4f2f09f3799423be16baad73d5edf23226815adf
Nicolas Villetelle <nicolas.villetelle@groupe-cyllene.com>
2020-11-04 16:19:48 +0100

    First Commit

---
+++ assets/js/RepLog/RepLogApp.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+
+export default class RepLogApp extends Component{
+    render() {
+        let emoji = '';
+        if (this.props.withHeart){
+            emoji = <span> 😱 </span>;
+        }
+
+            const repLogs = [
+                { id: 1, reps: 25, itemLabel: 'My Laptop', totalWeightLifted: 112.5 },
+                { id: 2, reps: 10, itemLabel: 'Big Fat Cat', totalWeightLifted: 180 },
+                { id: 8, reps: 4, itemLabel: 'Big Fat Cat', totalWeightLifted: 72 }
+            ];
+
+        return (
+            <div className="col-md-7">
+                <h2>Lift History ! {emoji}</h2>
+
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>What</th>
+                        <th>How many times?</th>
+                        <th>Weight</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {repLogs.map((repLog) => (
+                            <tr key={repLog.id}>
+                                <td>{repLog.itemLabel}</td>
+                                <td>{repLog.reps}</td>
+                                <td>{repLog.totalWeightLifted}</td>
+                                <td>...</td>
+                            </tr>
+                        )
+                    )}
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td>&nbsp;</td>
+                        <th>Total</th>
+                        <th>TODO</th>
+                        <td>&nbsp;</td>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>)
+    }
+}
---
+++ assets/js/rep_log_react.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import RepLogApp from "./RepLog/RepLogApp";
+
+const shouldShowEmoji = true;
+
+render(<RepLogApp withHeart={shouldShowEmoji}/>,
+    document.getElementById('lift-stuff-app'));
