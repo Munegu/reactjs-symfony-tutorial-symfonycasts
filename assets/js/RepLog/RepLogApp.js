1252de1fdc7820e54d4dabfdeb6648dd8e13a9ce
Nicolas Villetelle <nicolas.villetelle@groupe-cyllene.com>
2020-11-04 17:07:33 +0100

    First Commit

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,9 +1,27 @@
 import React, {Component} from "react";
 
 export default class RepLogApp extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            highlightedRowId: null,
+        }
+    }
+
+
+    handleRowClick (reLogId, event) {
+        this.setState({highlightedRowId: reLogId});
+    }
+
+
+
     render() {
+        const { highlightedRowId } = this.state;
+        const { withHeart } = this.props;
+
         let emoji = '';
-        if (this.props.withHeart){
+        if (withHeart){
             emoji = <span> 😱 </span>;
         }
 
@@ -28,7 +46,11 @@ export default class RepLogApp extends Component{
                     </thead>
                     <tbody>
                     {repLogs.map((repLog) => (
-                            <tr key={repLog.id}>
+                            <tr
+                                key={repLog.id}
+                                className={highlightedRowId === repLog.id ? 'info' : '' }
+                                onClick={(event) => this.handleRowClick(repLog.id, event)}
+                            >
                                 <td>{repLog.itemLabel}</td>
                                 <td>{repLog.reps}</td>
                                 <td>{repLog.totalWeightLifted}</td>
@@ -46,6 +68,36 @@ export default class RepLogApp extends Component{
                     </tr>
                     </tfoot>
                 </table>
+                <form className="form-inline">
+                    <div className="form-group">
+                        <label className="sr-only control-label required" htmlFor="rep_log_item">
+                            What did you lift?
+                        </label>
+                        <select id="rep_log_item"
+                                name="item"
+                                required="required"
+                                className="form-control">
+                            <option value="">What did you lift?</option>
+                            <option value="cat">Cat</option>
+                            <option value="fat_cat">Big Fat Cat</option>
+                            <option value="laptop">My Laptop</option>
+                            <option value="coffee_cup">Coffee Cup</option>
+                        </select>
+                    </div>
+                    {' '}
+                    <div className="form-group">
+                        <label className="sr-only control-label required" htmlFor="rep_log_reps">
+                            How many times?
+                        </label>
+                        <input type="number" id="rep_log_reps"
+                               name="reps" required="required"
+                               placeholder="How many times?"
+                               className="form-control"/>
+                    </div>
+                    {' '}
+                    <button type="submit" className="btn btn-primary">I Lifted it!</button>
+                </form>
+
             </div>)
     }
 }
